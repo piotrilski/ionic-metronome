ae563ef0d9cb13904b9189f24f0895df5cbff927
Piotr Ilski <piotr.ilski@gmail.com>
2017-01-03 12:30:31 +0100

    Update gulp-based build system

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const gulp = require('gulp');
 const gutil = require('gulp-util');
 const concat = require('gulp-concat');
@@ -9,62 +11,93 @@ const babel = require('gulp-babel');
 const templates = require('gulp-angular-templatecache');
 const uncache = require('gulp-uncache');
 const inject = require('gulp-inject');
+const jshint = require('gulp-jshint');
+const del = require('del');
 const sourcemaps = require('gulp-sourcemaps');
 const bower = require('bower');
 const sh = require('shelljs');
-const series = require('stream-series');
+const runSequence = require('run-sequence');
 
 const paths = {
   sass: ['./scss/**/*.scss'],
   es6: ['./src/**/*.js'],
-  html: ['./src/**/*.html']
+  html: ['./src/**/*.html'],
+  vendor: [
+    './vendor/**/*.*'
+  ],
+  dest: './www'
 };
 
-gulp.task('default', ['sass', 'babel', 'html']);
+gulp.task('default', ['build']);
 
-gulp.task('babel', function() {
-  return gulp.src(paths.es6)
+gulp.task('build', callback => {
+  runSequence(
+    'clean', 
+    'vendor',
+    ['sass', 'babel', 'html', 'index'], 
+    callback);
+});
+
+gulp.task('vendor', () => 
+  gulp.src(paths.vendor, { read: true })
+    .pipe(gulp.dest(paths.dest + '/lib')));
+
+gulp.task('lint', () =>
+  gulp.src(paths.es6)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish')));
+
+gulp.task('clean', () =>  del([paths.dest + '/**/*.*']));
+
+gulp.task('index', () => 
+  gulp.src('./src/index.html')
+    .pipe(gulp.dest(paths.dest)));
+
+gulp.task('babel', ['lint'], () => 
+  gulp.src(paths.es6)
     .pipe(sourcemaps.init())
     .pipe(plumber())
     .pipe(babel({ presets: ['es2015'] }))
     .pipe(concat('app.js'))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('www/js'));
-});
+    .pipe(gulp.dest(paths.dest + '/js')));
 
-gulp.task('html', function() {
-  return gulp.src(paths.html, { read: true })
+gulp.task('html', () => 
+  gulp.src(paths.html, { read: true })
     .pipe(templates('templates.js', { module: 'metronome', standalone: false }))
-    .pipe(gulp.dest('./www/js'));
-});
+    .pipe(gulp.dest(paths.dest + '/js')));
 
-gulp.task('sass', function(done) {
+gulp.task('vendor', () => 
+  gulp.src(paths.vendor + '/')
+    .pipe(gulp.dest(paths.dest + '/lib')));
+
+gulp.task('sass', done => {
   gulp.src('./scss/ionic.app.scss')
     .pipe(sass())
     .on('error', sass.logError)
-    .pipe(gulp.dest('./www/css/'))
+    .pipe(gulp.dest(paths.dest + '/css/'))
     .pipe(minifyCss({
       keepSpecialComments: 0
     }))
     .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
+    .pipe(gulp.dest(paths.dest + '/css/'))
     .on('end', done);
 });
 
-gulp.task('watch', ['sass'], function() {
+gulp.task('watch', ['build'], () => {
   gulp.watch(paths.es6, ['babel']);
   gulp.watch(paths.sass, ['sass']);
   gulp.watch(paths.html, ['html']);
+  gulp.watch('./src/index.html', ['index']);
 });
 
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
+gulp.task('install', ['git-check'], () => 
+  bower.commands.install()
     .on('log', function(data) {
       gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
-});
+    }));
 
-gulp.task('git-check', function(done) {
+gulp.task('git-check', done => {
   if (!sh.which('git')) {
     console.log(
       '  ' + gutil.colors.red('Git is not installed.'),
@@ -74,5 +107,6 @@ gulp.task('git-check', function(done) {
     );
     process.exit(1);
   }
+
   done();
 });
--- src/app.js
+++ src/app.js
@@ -20,6 +20,6 @@
 					StatusBar.styleDefault();
 				}
 			});
-		})
+		});
 
 })();
